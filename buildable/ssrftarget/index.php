352631772417ded0572327610646ba1382fa45da
nickcopi <32046105+nickcopi@users.noreply.github.com>
2023-07-13 21:40:23 +0000

    Start adding an ssrf target application to pivot to. Still need to get the query param checking to gate the flag working properly.

---
+++ buildable/ssrftarget/index.php
@@ -0,0 +1,6 @@
+This is a super secret internal page!
+Hit this page with query parameter of "getflag" to get the flag.
+<?php
+    echo "flag{internal_pivot_successful}";
+    echo $_REQUEST['getflag'];
+?>
