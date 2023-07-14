2e6fbe0e1ea43e403139d67d44be942a19ee6d13
nickcopi <32046105+nickcopi@users.noreply.github.com>
2023-07-14 00:17:00 +0000

    Get ssrf pivot target application delivering flag properly.

--- buildable/ssrftarget/index.php
+++ buildable/ssrftarget/index.php
@@ -1,6 +1,7 @@
 This is a super secret internal page!
 Hit this page with query parameter of "getflag" to get the flag.
 <?php
-    echo "flag{internal_pivot_successful}";
-    echo $_REQUEST['getflag'];
+    if (isset($_REQUEST['getflag'])) {
+        echo "flag{internal_pivot_successful}";
+    }
 ?>
